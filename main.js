fd7b650efc53e46a125e71333c7da611e5dbf4b2
Ronaldoss878 <98910579+Ronaldosdeltas@users.noreply.github.com>
2024-08-16 17:24:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function insert(num){
+const numero = document.getElementById('result'). innerHTML;
+document.getElementById('result').innerHTML = numero +num;
+}
+function clean(){
+    document.getElementById('result').innerHTML = '';
+}
+function back(){
+    const result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML =result.substring(0,result.length -1);
+}
+function calcular(){
+    const result =document.getElementById('result').innerHTML;
+   if(result){
+    document.getElementById('result').innerHTML = eval(result);
+   }
+
+}
+
